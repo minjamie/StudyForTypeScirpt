9784141067b8948667457fbad68c1225996d300e
minjaekim <minjae2246@gmail.com>
2022-02-08 22:04:34 +0900

    강의 1강 정리완료

---
+++ 이펙티브 타입스크립트/ch01/01/06.ts
@@ -0,0 +1,18 @@
+interface State {
+  name: string;
+  capital: string;
+}
+const states: State[] = [
+  { name: "Alabama", capitol: "Montgomery" },
+  { name: "Alaska", capitol: "Juneau" },
+  { name: "Arizona", capitol: "Phoenix" },
+  // ...
+];
+for (const state of states) {
+  console.log(state.capital);
+  // ~~~~~~~ Property 'capital' does not exist on type
+  //         '{ name: string; capitol: string; }'.
+  //         Did you mean 'capitol'?
+}
+
+export default {};
